51beaec84a52633a00016589acba39da53fb13d4
antonie <antonriziki@gmail.com>
2026-02-24 13:52:37 +0300

    added navbar routing pages to all templates

--- ReelRoots/ReelRoots_app/urls.py
+++ ReelRoots/ReelRoots_app/urls.py
@@ -5,15 +5,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('archive_details/', views.archive_details, name='archive_details'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('archive-details/', views.archive_details, name='archive_details'),
     path('chat/', views.chat, name='chat'),
     path('explore/', views.explore, name='explore'),
     path('upload/', views.new_upload, name='upload'),
     path('reels/', views.reels, name='reels'),
     # path("api/reels/", views.reels_api, name="reels_api"),
-    path('story_mode/', views.story_mode, name='story_mode'),
-    path('user_profile/', views.user_profile, name='user_profile'),
+    path('story-mode/', views.story_mode, name='story_mode'),
+    path('user-profile/', views.user_profile, name='user_profile'),
     path('animation/', views.animation, name='animation'),
     path('chatbot-response/', views.chatbot_response, name='chatbot-response')
 
